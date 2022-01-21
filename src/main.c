6cc6abd56de3147d974ce1579e381931f2ed3057
fanous69 <fanyvybiral@seznam.cz>
2022-01-21 08:28:39 +0100

    hotovo

--- src/main.c
+++ src/main.c
@@ -1,52 +1,68 @@
 #include "stm8s.h"
-#include "milis.h"
-
-/*#include "delay.h"*/
+#include "stm8s.h"
+#include "stm8_hd44780.h"
 #include <stdio.h>
-/*#include "uart1.h"*/
-
 #define _ISOC99_SOURCE
 #define _GNU_SOURCE
+#include "delay.h"
 
-#define LED_PORT GPIOC
-#define LED_PIN  GPIO_PIN_5
-#define LED_HIGH   GPIO_WriteHigh(LED_PORT, LED_PIN)
-#define LED_LOW  GPIO_WriteLow(LED_PORT, LED_PIN)
-#define LED_REVERSE GPIO_WriteReverse(LED_PORT, LED_PIN)
+void tim2_setup(void){
+     TIM2_TimeBaseInit(TIM2_PRESCALER_16, 7000 - 1 ); 
+    //TIM2_ITConfig(TIM2_IT_UPDATE, ENABLE);
+    TIM2_OC1Init(                // inicializujeme kanál 1 (TM2_CH1)
+        TIM2_OCMODE_PWM1,        // režim PWM1
+        TIM2_OUTPUTSTATE_ENABLE, // Výstup povolen (TIMer ovládá pin)
+        3000,                    // výchozí hodnota šířky pulzu (střídy) 1056/1600 = 66%
+        TIM2_OCPOLARITY_HIGH      // Polarita LOW protože LED rozsvěcím 0 (spol. anoda)
+     );
 
-#define BTN_PORT GPIOE
-#define BTN_PIN  GPIO_PIN_4
-#define BTN_PUSH (GPIO_ReadInputPin(BTN_PORT, BTN_PIN)==RESET) 
+    // ošetření nežádoucích jevů při změně PWM
+    TIM2_OC1PreloadConfig(ENABLE);
 
+    TIM2_Cmd(ENABLE);
+}
 
 void setup(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
-    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-    GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
 
-    init_milis();
-    /*init_uart1();*/
+    lcd_init(); //inicializace LCD
+
+    GPIO_Init(GPIOC,((GPIO_Pin_TypeDef)(GPIO_PIN_1 | GPIO_PIN_2)),GPIO_MODE_IN_PU_NO_IT); 
+    TIM1_DeInit();
+    TIM1_TimeBaseInit(16, TIM1_COUNTERMODE_UP, 500, 16); 
+    TIM1_EncoderInterfaceConfig(TIM1_ENCODERMODE_TI12,TIM1_ICPOLARITY_FALLING,TIM1_ICPOLARITY_FALLING);
+   TIM1_Cmd(ENABLE);
+
+   tim2_setup();
+
 }
 
 
 int main(void)
 {
-    uint32_t time = 0;
 
+    unsigned int present_value = 0x0000;
+    unsigned int previous_value = 0x0001;
+
+    char text[16];
     setup();
 
     while (1) {
+        
 
-        if (milis() - time > 333 && BTN_PUSH) {
-            LED_REVERSE; 
-            time = milis();
-            /*printf("%ld\n", time);*/
+//získávání aktuální hodnoty z enkoderu
+        present_value = TIM1_GetCounter();
+
+        if(present_value != previous_value)
+        {
+            lcd_gotoxy(0, 0);
+            sprintf(text,"regulace PWM %u ",present_value);
+            lcd_puts(text);
+            TIM2_SetCompare1(present_value);
         }
+        previous_value = present_value;
 
-        /*LED_REVERSE; */
-        /*_delay_ms(333);*/
-        /*printf("Funguje to!!!\n");*/
     }
 }
 
